bb3a2b7dc958e2cfd460bfb84f1c08615a31dc27
Nadirah Mohd Razak <nadirahmohdrazak@gmail.com>
2024-08-13 10:52:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAdzwMBHBMaBKNww4VuOEo44YJXUct-gEo",
+  apiKey: "Your API Key",
   authDomain: "nadirah-s.firebaseapp.com",
   databaseURL: "https://nadirah-s-default-rtdb.firebaseio.com",
   projectId: "nadirah-s",
